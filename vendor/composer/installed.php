0e7eedbb868885653d8f05a0c0fc360a1aec5a12
Marcelo Robson de A M da Silva <marcelorobson001@hotmail.com>
2022-02-28 22:58:46 -0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'marcelorobson001/phpmac-morpho',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'marcelorobson001/phpmac-morpho' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
